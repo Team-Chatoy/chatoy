1990825f5282d901c17fcd2caa17822355aaab7e
hyt <2677552163@qq.com>
2022-12-24 21:07:52 +0800

    登陆界面跳转注册界面

--- app/src/main/java/chatoy/Register.java
+++ app/src/main/java/chatoy/Register.java
@@ -1,71 +1,143 @@
 package chatoy;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Register extends JFrame implements ActionListener {
-  JLabel jbl1;
-  JTabbedPane jtp;
-  JPanel jp2, jp3, jp4;
-  JLabel jp2_jbl1, jp2_jbl2, jp2_jbl3;
-  JTextField jp2_jtf;
-  JPasswordField jp2_jpf;
-  JCheckBox jp2_jcb1, jp2_jcb2;
-  JPanel jp1;
-  JButton jp1_jb1;
-  JButton jp1_jb2;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
-  public static void main(String[] args) {
-    Register frame = new Register("Chatoy 登陆界面");
-    frame.setSize(400, 150);
-    frame.setVisible(true);
-  }
+public class Register extends JFrame {
+    private JPanel jpl1;
+    private JPanel jpl2;
+    private JPanel jpl3;
+    private JPanel jpl4;
+    private JLabel jlb1;
+    private JLabel jlb2;
+    private JLabel jlb3;
+    private JLabel jlb4;
+    private JTextField jtf;
+    private JPasswordField jpf;
+    private JCheckBox jcb;
+    private JTabbedPane jtp;
+    private JButton jbt1;
+    private JButton jbt2;
 
-  public Register(String str) {
-    super(str);
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+        }catch(Exception ee){
+            ee.printStackTrace();
+        }
 
-    jbl1 = new JLabel(new ImageIcon());
-    jp1 = new JPanel();
-    jp1_jb1 = new JButton("登录");
-    jp1_jb2 = new JButton("注册");
-    jp1.add(jp1_jb1);
-    jp1.add(jp1_jb2);
 
-    jp2 = new JPanel(new GridLayout(3, 3));
-    jp2_jbl1 = new JLabel("账号", JLabel.CENTER);
-    jp2_jbl2 = new JLabel("密码", JLabel.CENTER);
-    jp2_jbl3 = new JLabel("忘记密码", JLabel.CENTER);
-    jp2_jbl3.setForeground(Color.blue);
-    jp2_jtf = new JTextField();
-    jp2_jpf = new JPasswordField();
-    jp2_jcb1 = new JCheckBox("隐身登录");
-    jp2_jcb2 = new JCheckBox("记住密码");
+        Register frame = new Register();
 
-    jp2.add(jp2_jbl1);
-    jp2.add(jp2_jtf);
-    jp2.add(jp2_jbl2);
-    jp2.add(jp2_jpf);
-    jp2.add(jp2_jbl3);
-    jp2.add(jp2_jcb1);
-    jp2.add(jp2_jcb2);
+    }
 
-    jtp = new JTabbedPane();
-    jtp.add("账号", jp2);
-    jp3 = new JPanel();
-    jtp.add("手机号码", jp3);
-    jp4 = new JPanel();
-    jtp.add("电子邮件", jp4);
+    public Register(){
 
-    this.setSize(550, 340);
-    this.setIconImage((new ImageIcon()).getImage());
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setVisible(true);
-    this.add(jbl1, "North");
-    this.add(jp1, "South");
-    this.add(jtp, "Center");
-  }
+        Font font = new Font("Serif",Font.PLAIN,15);
+        ListenerOne listener = new ListenerOne();
+        jpl1 = new JPanel();
+        jpl1.setLayout(null);
+        jbt1 = new JButton("登录");
+        jbt2 = new JButton("注册");
+        jpl1.add(jbt1);
+        jpl1.add(jbt2);
+        jbt1.setBounds(50, 380, 190, 50);
+        jbt2.setBounds(260, 380, 190, 50);
+        jbt2.addActionListener(listener);
 
-  @Override
-  public void actionPerformed(ActionEvent e) { }
+
+        jpl2 = new JPanel();
+        jpl2.setLayout(null);
+        jlb1 = new JLabel("账号");
+        jlb2 = new JLabel("密码");
+        jtf = new JTextField();
+        jpf = new JPasswordField();
+        jcb = new JCheckBox("记住密码");
+        jcb.setFont(font);
+        jpl2.add(jlb1);
+        jpl2.add(jlb2);
+        jpl2.add(jtf);
+        jpl2.add(jpf);
+        jpl2.add(jcb);
+        jlb1.setBounds(50, 20, 300, 20);
+        jlb2.setBounds(50, 120, 300, 20);
+        jtf.setBounds(50, 60, 300, 30);
+        jpf.setBounds(50, 160, 300, 30);
+        jcb.setBounds(50, 230, 300, 30);
+        jtp = new JTabbedPane();
+        jtp.addTab("账号", jpl2);
+
+        jpl3 = new JPanel();
+        jpl3.setLayout(null);
+        jlb3 = new JLabel("电话号码");
+        jlb2 = new JLabel("密码");
+        jtf = new JTextField();
+        jpf = new JPasswordField();
+        jcb = new JCheckBox("记住密码");
+        jcb.setFont(font);
+        jpl3.add(jlb3);
+        jpl3.add(jlb2);
+        jpl3.add(jtf);
+        jpl3.add(jpf);
+        jpl3.add(jcb);
+        jlb3.setBounds(50, 20, 300, 20);
+        jlb2.setBounds(50, 120, 300, 20);
+        jtf.setBounds(50, 60, 300, 30);
+        jpf.setBounds(50, 160, 300, 30);
+        jcb.setBounds(50, 230, 300, 30);
+        jtp.addTab("电话号码", jpl3);
+
+        jpl4 = new JPanel();
+        jpl4.setLayout(null);
+        jlb4 = new JLabel("电子邮件");
+        jlb2 = new JLabel("密码");
+        jtf = new JTextField();
+        jpf = new JPasswordField();
+        jcb = new JCheckBox("记住密码");
+        jcb.setFont(font);
+        jpl4.add(jlb4);
+        jpl4.add(jlb2);
+        jpl4.add(jtf);
+        jpl4.add(jpf);
+        jpl4.add(jcb);
+        jlb4.setBounds(50, 20, 300, 20);
+        jlb2.setBounds(50, 120, 300, 20);
+        jtf.setBounds(50, 60, 300, 30);
+        jpf.setBounds(50, 160, 300, 30);
+        jcb.setBounds(50, 230, 300, 30);
+        jtp.addTab("电子邮件", jpl4);
+
+        jpl1.add(jtp);
+        jtp.setBounds(50, 50, 400, 300);
+        this.add(jpl1);
+        this.setTitle("Chatoy登陆界面");
+        this.setSize(500,500);
+        this.setVisible(true);
+
+
+    }
+
+    class ListenerOne implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == jbt2) {
+                chatoy.Login login = new chatoy.Login();
+            }
+
+        }
+
+    }
+
 }
