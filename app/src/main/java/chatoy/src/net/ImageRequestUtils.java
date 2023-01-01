42fc118b9c2434f0cb31df051b6a1c1bedd9bdbe
beggarYF <862197091@qq.com>
2023-01-01 23:29:55 +0800

    change

---
+++ app/src/main/java/chatoy/src/net/ImageRequestUtils.java
@@ -0,0 +1,12 @@
+package net;
+
+import java.awt.*;
+import java.io.Closeable;
+
+public class ImageRequestUtils {
+    // 有参方式
+    public static Image getImage(String url) {
+        // CloseAble
+        return null;
+    }
+}
