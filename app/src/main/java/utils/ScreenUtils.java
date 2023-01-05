21e7831fe9498f916944443cbca5e2df7c45273f
yurzhang <yurzhang@163.com>
2023-01-05 20:34:45 +0800

    fix: component & utils

--- app/src/main/java/utils/ScreenUtils.java
+++ app/src/main/java/utils/ScreenUtils.java
@@ -1,13 +1,13 @@
 package utils;
 
-import java.awt.*;
+import java.awt.Toolkit;
 
 public class ScreenUtils {
-    public static int getScreenWidth() {
-        return Toolkit.getDefaultToolkit().getScreenSize().width;
-    }
+  public static int getScreenWidth() {
+    return Toolkit.getDefaultToolkit().getScreenSize().width;
+  }
 
-    public static int getScreenHeight() {
-        return Toolkit.getDefaultToolkit().getScreenSize().height;
-    }
+  public static int getScreenHeight() {
+    return Toolkit.getDefaultToolkit().getScreenSize().height;
+  }
 }
